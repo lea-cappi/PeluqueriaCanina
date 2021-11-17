1b63d195cec7feef3b3d32b84fe2d5b08d39abe9
lea-cappi <leandrocappiello@gmail.com>
2021-11-17 20:50:01 -0300

    volviendo atras, me quedo con la secuencia de a 50..

--- src/logica/Cliente.java
+++ src/logica/Cliente.java
@@ -7,18 +7,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 
 //clase entidad, relacion o tabla en base de datos
 @Entity
 public class Cliente implements Serializable {
     
     //declaracion de atributos de la clase
-    //primary key y generador de secuencia de autoincremento en 1
-    @SequenceGenerator(name="incrementoEnUno", allocationSize=1)
+    //primary key    
     @Id
-    //autoincremental secuenciado en generador de secuencia
-    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="incrementoEnUno")
+    //autoincremental
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int idCliente;
     //atributos o campos
     @Basic
--- src/logica/PeluqueriaCanina.java
+++ src/logica/PeluqueriaCanina.java
@@ -8,22 +8,7 @@ public class PeluqueriaCanina {
     public static void main(String[] args) {
         //llamada a la controladora de logica
         Controladora control = new Controladora ();
-        //prueba creando un cliente mediante constructor vacio para testear idCliente
-        Cliente cli = new Cliente ();
-        //seteo de atributos
-        cli.setNumCliente(7635);
-        cli.setNombrePerro("Wimpy");
-        cli.setRaza("Cooker");
-        cli.setColor("Blanco y negro");
-        cli.setAlergico("true");
-        cli.setAtencionEsp("true");
-        cli.setNombreDuenio("Monica");
-        cli.setCelDuenio("642345");
-        cli.setObservaciones("Medio insoportable diria un tuerto");
         
-        
-        //llamada a controladora
-        control.altaCliente(cli);
         
     }
     
