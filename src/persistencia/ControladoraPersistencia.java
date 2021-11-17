d9fa064a2cfa19dd6f9f27d7c45ab4ea09013171
lea-cappi <leandrocappiello@gmail.com>
2021-11-17 19:30:28 -0300

    llamada e instanciado a ClienteJpaController con metodo constructor que setea UP en ControladoraPersistencia. Prueba crear tabla Cliente en BD

--- src/persistencia/ControladoraPersistencia.java
+++ src/persistencia/ControladoraPersistencia.java
@@ -3,5 +3,7 @@ package persistencia;
 
 
 public class ControladoraPersistencia {
+    //llamada e instanciado a ClienteJpaController con metodo constructor que setea UP
+    ClienteJpaController cliJPA = new ClienteJpaController ();
     
 }
