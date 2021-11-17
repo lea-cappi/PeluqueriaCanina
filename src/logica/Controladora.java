e03f09eaa2e886ec0af87ff6cd7a425d4d62e299
lea-cappi <leandrocappiello@gmail.com>
2021-11-17 19:51:26 -0300

    creados metodos altaCliente en ambas Controladoras, instanciado a Cliente en Controladora para prueba de persistencia. Cruzemos los dedos

--- src/logica/Controladora.java
+++ src/logica/Controladora.java
@@ -7,4 +7,16 @@ import persistencia.ControladoraPersistencia;
 public class Controladora {
     //llamar a la controladora de la persistencia
     ControladoraPersistencia controlPersis = new ControladoraPersistencia ();
+    
+    //prueba instanciando un cliente
+    Cliente cli = new Cliente (1, 3241,"Leyla", "Terbal", "Castano", "false", "false", "Leandro", "2345234", "La mas bella del mundo");
+    
+    
+    
+    //pivot con metodo alta cliente de persistencia
+    public void altaCliente (Cliente cli) {
+        controlPersis.altaCliente(cli);
+    }
+    
+    
 }
--- src/persistencia/ControladoraPersistencia.java
+++ src/persistencia/ControladoraPersistencia.java
@@ -1,9 +1,17 @@
 
 package persistencia;
 
+import logica.Cliente;
+
 
 public class ControladoraPersistencia {
     //llamada e instanciado a ClienteJpaController con metodo constructor que setea UP
     ClienteJpaController cliJPA = new ClienteJpaController ();
+    
+    //metodo de alta
+    public void altaCliente (Cliente cli) {
+        cliJPA.create(cli);
+    
+    }
     
 }
