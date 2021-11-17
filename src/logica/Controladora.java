1a2128f481c129fff1438d4b8f175c641ea34515
lea-cappi <leandrocappiello@gmail.com>
2021-11-17 19:22:55 -0300

    llamada a la contr de persis mediante instancia cuando sea llamada la controladora en Controladora. Previo prueba de creacion de tabla Cliente

--- src/logica/Controladora.java
+++ src/logica/Controladora.java
@@ -1,7 +1,10 @@
 
 package logica;
 
+import persistencia.ControladoraPersistencia;
+
 
 public class Controladora {
-    
+    //llamar a la controladora de la persistencia
+    ControladoraPersistencia controlPersis = new ControladoraPersistencia ();
 }
